fcd007adab5ad02e0b102b80e330244483bac046
MOOW4 <108760540+MOOW4@users.noreply.github.com>
2023-06-20 23:41:34 +0200

    remove comments

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -4,7 +4,10 @@ import fs from "fs";
 
 /* 
 1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
 */
+
 inquirer
 .prompt([
     /* Pass your questions in here */
@@ -36,7 +39,3 @@ inquirer
     }
 });
 
-/*
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
